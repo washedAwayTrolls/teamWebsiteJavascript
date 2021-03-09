16067d667aa2046fafecda1337a2454fc579e1bd
Nathan Farrell <nathanfarrell188@u.boisestate.edu>
2021-03-08 17:57:28 -0700

    button now changes a picture

--- random.js
+++ random.js
@@ -1,9 +1,16 @@
 var myButton = document.getElementById('buttonTest');
-
-
+var changingPicture = document.getElementById("changingPicture");
+var div = document.getElementById("div1");
 
 
 
     function buttonPressed() {
         myButton.innerHTML = "You changed it!";
+        changingPicture.src = "ant.gif";
     }
+
+    function giveDivBorder() {
+    
+    }
+
+    
