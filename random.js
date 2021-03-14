01b8b630538f48615544c7581d2527a3e12715c4
Nathan Farrell <nathanfarrell188@u.boisestate.edu>
2021-03-14 16:17:42 -0600

    attempting rng

--- random.js
+++ random.js
@@ -4,8 +4,12 @@ var div = document.getElementById("div1");
 var checkTextBoxButton = document.getElementById("checkTextBox");
 var text1 = document.getElementById("text1");
 var generateNum = document.getElementById("generateNumButton");
-var rangeNum1 = document.getElementById("rangeNum1");
-var rangeNum2 = document.getElementById("rangeNum2");
+var rangeNum1 = document.getElementById("numRange1").value;
+var rangeNum2 = document.getElementById("numRange2").value;
+var randomNum;
+rangeNum1 = parseInt(rangeNum1);
+rangeNum2 = parseInt(rangeNum2);
+
     function buttonPressed() {
         myButton.innerHTML = "You changed it!";
         if (changingPicture.src == "chao.png") {
@@ -28,7 +32,10 @@ var rangeNum2 = document.getElementById("rangeNum2");
     };
 
     generateNum.onclick = function() {
-        generateNum.innerHTML = Math.random() * (rangeNum2 - rangeNum1) + rangeNum1;
+        var randomNum = Math.random() * (rangeNum2 - rangeNum1) + rangeNum1;
+        generateNum.innerHTML = toString(randomNum);
+        
+        console.log(randomNum);
     };
 
     /*window.onload = function {
