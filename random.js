1bba7250d297fbe9fae72f4ee8d86491f25bc63e
Nathan Farrell <nathanfarrell188@u.boisestate.edu>
2021-03-22 10:58:22 -0600

    finished random.html (for the most part)

--- random.js
+++ random.js
@@ -2,15 +2,15 @@
 var div = document.getElementById("div1");
 var checkTextBoxButton = document.getElementById("checkTextBox");
 var text1 = document.getElementById("text1");
-var generateNum = document.getElementById("generateNumButton");
+
 
 var awfulLuckAudio = new Audio("awfulluck.wav");
+var amazingLuckAudio = new Audio("amazingluck.wav");
 
 
 
 
-var submitValue = document.getElementById("genNumSubmit");
-var min = document.getElementById("min");
+
 //rangeNum1 = parseInt(rangeNum1);
 //rangeNum2 = parseInt(rangeNum2);
 
@@ -48,24 +48,31 @@ var changingPicture = document.getElementById("changingPicture");
 
     function generateRandomNumber() {
         var rangeNum1 = document.getElementById("numRange1");
+        var rn1 = parseInt(rangeNum1.value);
         var rangeNum2 = document.getElementById("numRange2");
+        var rn2 = parseInt(rangeNum2.value);
         var yourRandomNumber = document.getElementById("yourRandomNumber");
         var randomNum = 0;
-
+        console.log("rn1: " + rn1);
+        console.log("rn2: " + rn2);
+        var median = ((rn1 + rn2) / 2);
+        console.log("median: " + median);
+        var median2 = rn1 + rn2;
+        console.log("median2test: " + median2);
         
 
-        //var randomNumpart1 = Math.floor(Math.random() * 10 + 1);
-        console.log(randomNum);
+        
+        
         console.log("Min: " + rangeNum1.value);
         console.log("Max: " + rangeNum2.value);
        // randomNum = getRandomNumber(rangeNum1, rangeNum2);
 
        randomNum = Math.floor((Math.random() * rangeNum2.value) + rangeNum1.value);
-       console.log(randomNum)
-       while (randomNum > rangeNum2.value || randomNum == 0 || randomNum == 1) {
+       
+       while (randomNum > rangeNum2.value || randomNum == 0 || randomNum == 1 || randomNum < rangeNum1.value) {
            
         randomNum = Math.floor((Math.random() * rangeNum2.value) + rangeNum1.value);
-        console.log(randomNum);
+        console.log("random Number: " + randomNum);
 
         console.log("Min: " + rangeNum1.value);
         console.log("Max: " + rangeNum2.value);
@@ -75,27 +82,24 @@ var changingPicture = document.getElementById("changingPicture");
 
         yourRandomNumber.innerHTML = "Your random number is: " + randomNum;
 
-        if (randomNum < (rangeNum2.value + rangeNum1.value) / 2) {
+        
+
+        if (randomNum < median) {
+            console.log("Mean median: " + median);
             awfulLuckAudio.play();
+        } else {
+            amazingLuckAudio.play();
         }
 
-       
+       console.log("rangeNum2: " + rangeNum2.value + "rangeNum1: " + rangeNum1.value);
+
         
         console.log(randomNum);
         console.log(rangeNum1.value + " " + rangeNum2.value);
     }
 
-    function getRandomNumber(min, max) {
-        
-        let randNumber = Math.floor((Math.random() * max) + min);
-        while (randNumber > max) {
-            randNumber = Math.floor((Math.random() * max) + min);
-        }
-        return randNumber;
-    }
+    
 
-    /*window.onload = function {
-        
-    }*/
+    
 
     
